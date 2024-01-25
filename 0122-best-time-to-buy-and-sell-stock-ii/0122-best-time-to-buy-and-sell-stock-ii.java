d5f8a90e1981b8b8467f76b2b949f600e9d2945c
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-01-25 16:44:42 +0530

    Time: 4 ms (7.06%), Space: 45.5 MB (56.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int ans = 0;
-        for (int i = 1; i < prices.length; i += 1) {
-            ans += Math.max(0, prices[i] - prices[i - 1]);
-        }
-        return ans;
+        return IntStream.range(1, prices.length).map(i -> Math.max(0, prices[i] - prices[i - 1])).sum();
     }
 }
