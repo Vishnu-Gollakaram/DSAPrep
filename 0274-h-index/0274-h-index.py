a86e96aabe673dcf950114cb0b546e38d427d1b2
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-01-26 11:07:34 +0530

    Time: 14 ms (93.64%), Space: 11.9 MB (96.87%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
+        # if current citation value > rem count
+        # return rem citations + 1 (total value)
         citations.sort()
         l = len(citations)
         for i in range(0 , l):
