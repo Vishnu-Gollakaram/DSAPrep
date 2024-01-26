6df9287f125a57d62a5be03430cef07caf07235d
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-01-27 01:22:00 +0530

    Time: 936 ms (85.41%), Space: 22.1 MB (75.15%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,3 +1,26 @@
+''' # My approach 0(n^2) TLE
+class Solution:
+    def canReach(self, ind, arr, cost):
+        start = ind
+        end = start - 1
+        length = len(arr)
+        if end < 0:
+            end = length - 1
+        curReach = 0
+        while ind % length != end:
+            curReach += arr[ind % length] - cost[ind % length]
+            if curReach <= 0:
+                return False
+            ind += 1
+        return curReach + arr[end] - cost[end] >= 0
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        length = len(gas)
+        for i in range(length):
+            if self.canReach(i, gas, cost):
+                return i
+        return -1
+'''
+
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         total_gas = 0
