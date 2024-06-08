127433d521b8151c9833404022102cb2d96c1812
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-06-08 17:17:01 +0530

    Time: 649 ms (79.01%), Space: 52.2 MB (66.07%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,12 +1,8 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        n = 0
-        for edge in edges:
-            n = max(edge[0], edge[1], n)
-        reachable = [0] * (n + 1)
-        for edge in edges:
-            reachable[edge[0]] += 1
-            reachable[edge[1]] += 1
-        for ind in range(n + 1):
-            if reachable[ind] == n - 1:
-                return ind
+        # In a star graph, the center will appear in both the first two edges
+        # Return the common node in the first two edges
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
