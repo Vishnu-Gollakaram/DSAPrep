f02c50b9439b6f719b2bbc7b13293c8371dc5378
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-10-10 10:01:04 +0530

    Time: 30 ms (82.81%), Space: 16.5 MB (39.81%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -1,4 +1,16 @@
 class Solution:
+    '''
+    0
+    0 | 1
+    01 | 10
+    0110 | 1001
+
+    As one can observe, row n + 1 is copy of row n in first half and compelent of row n in  second half. 
+
+    Hence, the idea is we can reduce: n, k such that it goes down to n = 1, k = 1 (base case)
+
+    If k comes after mid we would return 1's compelemt of value or we would return k
+    '''
     def kthGrammar(self, n: int, k: int) -> int:
 
         def k_helper(n, k):
