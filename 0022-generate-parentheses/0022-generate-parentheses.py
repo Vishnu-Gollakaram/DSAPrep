4325508a08440e06be8d84882aa0e9b704e33cbd
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-10-14 01:17:26 +0530

    Time: 40 ms (38.8%), Space: 16.8 MB (87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,16 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        def gen_braces(n, pattern, op, cl, max_op):
-            if n == 0:
-                ans.append(pattern)
+        
+        def gen_braces(current, op, cl):
+            # Base case: if the length of the current list is 2*n, append the pattern as a string
+            if len(current) == 2 * n:
+                ans.append("".join(current))
                 return
             
-            if op < max_op:
-                gen_braces(n - 1, pattern + "(", op + 1, cl, max_op)
-
+            # Add an opening parenthesis if we still have available ones
+            if op < n:
+                current.append("(")
+                gen_braces(current, op + 1, cl)
+                current.pop()  # Backtrack
+            
+            # Add a closing parenthesis if the number of closing is less than opening
             if cl < op:
-                gen_braces(n - 1, pattern + ")", op, cl + 1, max_op)
-
-        gen_braces(n * 2, "", 0, 0, n)
+                current.append(")")
+                gen_braces(current, op, cl + 1)
+                current.pop()  # Backtrack
+        
+        gen_braces([], 0, 0)
         return ans
