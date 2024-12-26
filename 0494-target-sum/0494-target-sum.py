6a04537ac4bd3962c27ad119820fad1ca4be5712
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2024-12-27 00:19:06 +0530

    Time: 200 ms (23.9%), Space: 38.5 MB (24.98%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -4,13 +4,18 @@ class Solution:
         memo = {}  # Use a dictionary to store calculated results
 
         def rec(i, su):
-            if i == n:
+
+            if (i, su) in memo:
+                return memo[(i, su)]
+
+            elif i == n:
                 if su == target:
                     return 1
                 return 0
 
-            if (i, su) not in memo:  # Check if the result is already calculated
+            else:
                 memo[(i, su)] = rec(i + 1, su + nums[i]) + rec(i + 1, su - nums[i])
+            
             return memo[(i, su)]
 
         return rec(0, 0)
