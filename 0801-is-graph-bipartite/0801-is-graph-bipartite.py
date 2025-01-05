ad1216e204a4cc0baa22e5abbdedc076efa7f3a6
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-01-05 23:43:02 +0530

    Time: 12 ms (9.78%), Space: 18.6 MB (10.54%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,16 +1,24 @@
+from collections import deque
+
 class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        colors = [None] * len(graph)
-        for i in range(len(graph)):
-            if colors[i] is None:
-                colors[i] = 1
-                queue = [i]
-                while queue:
-                    currentNode = queue.pop(0)
-                    for node in graph[currentNode]:
-                        if colors[node] is None:
-                            colors[node] = 1 - colors[currentNode]
-                            queue.append(node)
-                        elif colors[node] == colors[currentNode]:
+    def isBipartite(self, adj):
+        q = deque()
+        n = len(adj)
+        vis = [False] * n  # -1: not visited, 0: color A, 1: color B
+
+        for node in range(n):
+            if vis[node] == False:  # Unvisited node
+                vis[node] = 0  # Assign color A
+                q.append((node, 0))
+                
+                while q:
+                    cur, to_set = q.popleft()
+                    
+                    for neigh in adj[cur]:
+                        if vis[neigh] == False:  # Not visited
+                            vis[neigh] = 1 - to_set  # Assign opposite color
+                            q.append((neigh, 1 - to_set))
+                        elif vis[neigh] != (1 - to_set):  # Conflict in coloring
                             return False
+
         return True
