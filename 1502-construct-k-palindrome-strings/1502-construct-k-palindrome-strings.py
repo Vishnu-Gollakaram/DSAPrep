de38119dacbe5d9fed65108c770e797f95ceaec5
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-01-11 19:46:28 +0530

    Time: 45 ms (31.28%), Space: 18.2 MB (8.81%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -2,8 +2,8 @@ class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         n=len(s)
         if n<k: return False
-        freq=[False]*26
+        freq=[0]*26
         for c in s:
             freq[ord(c)-97]^=1
-        return freq.count(True)<=k
+        return freq.count(1)<=k
         
