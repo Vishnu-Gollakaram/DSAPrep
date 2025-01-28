d050755a064cf2c5076ef276bf6a686c58df0750
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-01-28 14:12:36 +0530

    Time: 35 ms (73.41%), Space: 17.9 MB (63.41%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.py
@@ -7,20 +7,20 @@ class Solution:
         dy = [1, -1, 0, 0]
 
         def dfs(r, c):
-            val = grid[r][c]
+            val = grid[r][c] # Fish in current location
             grid[r][c] = 0
             for k in range(4):
                 nx = r + dx[k]
                 ny = c + dy[k]
 
                 if -1 < nx < m and -1 < ny < n and grid[nx][ny] > 0:
-                    val += dfs(nx, ny)
+                    val += dfs(nx, ny) # Also grad fish from all neighbours reachable
 
             return val
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] > 0:
-                    ans = max(dfs(i, j), ans)
+                    ans = max(dfs(i, j), ans) # Find max reachable fish cluster
 
         return ans
