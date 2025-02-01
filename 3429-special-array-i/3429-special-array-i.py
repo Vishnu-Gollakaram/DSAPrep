a77e11e118267673945862cb0a7bdae1cadb6bec
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-02-01 19:47:36 +0530

    Time: 0 ms (100%), Space: 18 MB (12.54%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -1,10 +1,20 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        n = len(nums)
-        prev = nums[0] % 2
-        for i in range(1, n):
-            cur = nums[i] % 2
-            if prev == cur:
+        # Variables to be used later
+        size_of_nums = len(nums)
+        previous_element_parity = nums[0] & 1
+
+        # Idea is to check for parity equality across array
+        # Parity is just seeing if element is even or odd
+        # Dividing into 2 element running stream
+        # Compare Element 0 with element 1, then 1 with 2 and so on
+        # If any 2 elements have same parity (previous_element_parity == current_element_parity) return False
+        # If no matching parity found, nums is special array. So, return True
+        for index in range(1, size_of_nums):
+            current_element_parity = nums[index] & 1 # Famous syntax is n % 2 to check even or odd, but this is more time efficient as single bit comparision.
+
+            if previous_element_parity == current_element_parity:
                 return False
-            prev = cur
+            previous_element_parity = current_element_parity
+
         return True
