093ce3c5ba0978f22cf31ce92a4bee473e8af80b
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-02-02 14:28:40 +0530

    Time: 0 ms (100%), Space: 41.3 MB (50.38%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -3,7 +3,14 @@ class Solution {
         /*
             We need to <=1 break as we are searching for rotated sorted array
             Breaks stores count of number of unsorted parts (LEFT ELEMENT > RIGHT)
-            For a roated array, at max there is only one unsorted part
+            For a roated array, at max there is only one unsorted part, hence false
+            if array is not roated, it's true
+            If array is rotated, there are 2 parts, left and right
+            left part should always be greater than right (min_pre >= max_post)
+            Hence, 3 conditions:
+            1) If more than 1 break, return false
+            2) If no breaks, return true
+            3) If 1 break, check left side minimum (0th element) is greater than or equal to right side maxima (n - 1 th element)
         */
         int breaks = 0;
         int length_of_nums = nums.length;
