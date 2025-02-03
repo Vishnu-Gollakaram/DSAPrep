59d5e1300e02ad5b358643ec8141833f156afdd0
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-02-03 20:24:55 +0530

    Time: 0 ms (100%), Space: 8.9 MB (90.91%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.c
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.c
@@ -1,28 +1,36 @@
 int longestMonotonicSubarray(int* nums, int numsSize) {
-    int ans = 0;
-    int dec = 0;
-    int inc = 0;
+    // Intialise all Sequences to 0
+    int maximum_sequence = 0;
+    int decreasing_sequence = 0;
+    int increasing_sequence = 0;
+
+    // Count for both increasing and decreasing sequences
+    // If increasing value found increment increasing_sequence
+    // If decreasing value found increment decreasing_sequence
+    // Set the other variable to 0 in other cases
     for (int i = 1; i < numsSize; i++) {
         if (nums[i] > nums[i - 1]) {
-            inc++;
-            dec = 0;
+            increasing_sequence++;
+            decreasing_sequence = 0;
         }
         else if (nums[i] == nums[i - 1]) {
-            dec = 0;
-            inc = 0;
+            decreasing_sequence = 0;
+            increasing_sequence = 0;
         }
         else {
-            dec++;
-            inc = 0;
+            decreasing_sequence++;
+            increasing_sequence = 0;
         }
-        if (inc > ans) {
-            ans = inc;
+        if (increasing_sequence > maximum_sequence) {
+            maximum_sequence = increasing_sequence;
         }
 
-        if (dec > ans) {
-            ans = dec;
+        // In each Iteration, trace maximum value via check
+        if (decreasing_sequence > maximum_sequence) {
+            maximum_sequence = decreasing_sequence;
         }
     }
 
-    return ans + 1;
+    // We need to return sequence length not sequnce hence always +1 added.
+    return maximum_sequence + 1;
 }
