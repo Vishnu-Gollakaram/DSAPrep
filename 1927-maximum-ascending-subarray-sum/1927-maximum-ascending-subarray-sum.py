0b95ffa62beaa7ca2ddc1900ed203b783ffa7775
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-02-05 00:03:37 +0530

    Time: 0 ms (100%), Space: 17.8 MB (44.25%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
@@ -4,9 +4,11 @@ class Solution:
         maxSum = nums[0]
         current_pattern = nums[0]
         for i in range(1, len(nums)):
-            if nums[i] > nums[i-1]:
-                current_pattern += nums[i]
+            previous_element = nums[i - 1]
+            current_element = nums[i]
+            if current_element > previous_element:
+                current_pattern += current_element
             else:
-                current_pattern = nums[i]
+                current_pattern = current_element
             maxSum = max(maxSum,current_pattern)
         return maxSum
