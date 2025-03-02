3f64016ad657fd6e7374f231e4361758f8afa9fd
Gollakaram Vishnu Srivardhan <vishnugollakaram2021@gmail.com>
2025-03-02 13:32:02 +0530

    Time: 0 ms (100%), Space: 17.8 MB (99.24%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -1,18 +1,21 @@
+from collections import deque
+
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        ans = []
+        ans = deque()
         while nums1 and nums2:
-            if nums1[0][0] == nums2[0][0]:
-                ans.append([nums1[0][0], nums1[0][1] + nums2[0][1]])
-                nums1.pop(0)
-                nums2.pop(0)
-            elif nums1[0][0] < nums2[0][0]:
-                ans.append(nums1.pop(0))
+            if nums1[-1][0] == nums2[-1][0]:
+                ans.appendleft([nums1[-1][0], nums1[-1][1] + nums2[-1][1]])
+                nums1.pop()
+                nums2.pop()
+            elif nums1[-1][0] > nums2[-1][0]:
+                ans.appendleft(nums1.pop())
             else:
-                ans.append(nums2.pop(0))
+                ans.appendleft(nums2.pop())
+        ans = list(ans)
         if nums1:
-            ans += nums1
+            ans = nums1 + ans
         if nums2:
-            ans += nums2
+            ans = nums2 + ans
 
         return ans
